bf8372c3ccdb67bf9df090d7c757ab2ac0063808
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-19 20:54:00 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,11 +4,13 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/pulse(Telkomsel):*_ 082287219167
-_*Dana/ovo:*_ 0895612153565
-Setelah melakukan donasi kirim bukti pembayaran ke owner
+• Pulsa Indosat [085784715971]
+• Dana [085784715971]
+• Ovo [085784715971]
+• Shopeepay [085784715971]
+• Gopay [085784715971]
 `
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://chat.whatsapp.com/HQ3Jb07y3cI8NQBCqZGYfH/', 'Grup', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 
