be9e86c4068f80ba9184dacbcc5948bb5c3fdeab
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-19 20:54:58 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
 let str = `*https://youtube.com/channel/UCYZ3s2hcv-3uYuR-BuO6wqg*\nJangan Lupa Subscribe`
-conn.sendHydrated(m.chat, str, wm, pp, 'https://chat.whatsapp.com/HQ3Jb07y3cI8NQBCqZGYfH', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, 'https://chat.whatsapp.com/HQ3Jb07y3cI8NQBCqZGYfH/', 'Grup', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 }
